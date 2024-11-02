6fb89516db49f6cce8a9fd4bd12216f0a1144287
Fishybearr <76235767+Fishybearr@users.noreply.github.com>
2024-11-02 12:59:43 -0400

    Reorganize project structure and create priorities list

---
+++ client/client.js
@@ -0,0 +1,12 @@
+/* TODO:
+1) rebuild system for fecthing images
+
+2) set up apache to execute python with cgi for backend management and image uploading
+
+3) build timeline system for viewing images
+
+4) dynamic loading and unloading of images based scroll location on page
+
+5) place holder loading icons for images
+
+*/
